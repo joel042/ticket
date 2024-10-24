188eb056cfa1cc1bdf4ddf69572555d3771fbe06
joel042 <uchennajoeleze@gmail.com>
2024-10-24 21:05:15 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './Component/NavBar';
+
+function App() {
+  return (
+    <>
+      <div className=''>
+      <NavBar />
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
