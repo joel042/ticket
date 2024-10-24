129695c97de7e6cece27741212db28732f9b0400
joel042 <uchennajoeleze@gmail.com>
2024-10-24 21:18:24 +0100

    .

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-      <div>
-        
+      <div className="flex justify-between items-center p-4 text-white bg-[#1f262d] h-[50px]">
+        <img className='w-10'
+          src="https://img.icons8.com/?size=100&id=9433&format=png&color=ffffff"
+          alt=""
+        />
+
+        <h1>My Tickets</h1>
+        <h1>Help</h1>
       </div>
     </div>
-  )
+  );
 }
 
 export default NavBar
